4d53dfad86ae60973245ed9a362cdd10fb69153f
suvrajit <suvrajit.arowana@gmail.com>
2025-01-10 23:41:18 +0530

    updated api

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-// API route for Google Login
-Route::post('/login/google', [AuthController::class, 'googleLogin']);
-
-// Example protected route
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working!']);
