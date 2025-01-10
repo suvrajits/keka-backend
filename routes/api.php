1b58162ed1b86f0592293a4b2c844ae25caea94f
suvrajit <suvrajit.arowana@gmail.com>
2025-01-11 00:20:46 +0530

    updated task route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::get('/test', function () { return response()->json(['message' => 'API is 
     working!']);
 });
+
+Route::get('/task',[TaskController::class,'index']);
