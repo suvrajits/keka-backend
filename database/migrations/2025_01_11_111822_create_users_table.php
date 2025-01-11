5d8388520ee6157c61ea77e4e87059f39bc8ce54
suvrajit <suvrajit.arowana@gmail.com>
2025-01-11 16:53:36 +0530

    updated create user migration file

--- database/migrations/2025_01_11_111822_create_users_table.php
+++ database/migrations/2025_01_11_111822_create_users_table.php
@@ -6,21 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('google_id')->unique();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->text('avatar')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
