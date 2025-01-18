97e77cbfa4595e8dbe9df50c2f202dafc9c391b4
suvrajit <suvrajit.arowana@gmail.com>
2025-01-18 15:52:08 +0530

    updated

--- database/migrations/2025_01_18_101849_add_track_id_to_scores_table.php
+++ database/migrations/2025_01_18_101849_add_track_id_to_scores_table.php
@@ -4,25 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddTrackIdToScoresTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('scores', function (Blueprint $table) {
-            //
+            $table->uuid('track_id')->unique()->after('score'); // Add the track_id field
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::table('scores', function (Blueprint $table) {
-            //
+            $table->dropColumn('track_id'); // Rollback by dropping the track_id column
         });
     }
-};
+}
