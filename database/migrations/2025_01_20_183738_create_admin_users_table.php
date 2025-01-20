b72b4f0bd6f803c86748a3eae9f0336f86231fa9
suvrajit <suvrajit.arowana@gmail.com>
2025-01-21 00:09:15 +0530

    updated

--- database/migrations/2025_01_20_183738_create_admin_users_table.php
+++ database/migrations/2025_01_20_183738_create_admin_users_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('admin_users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
@@ -20,7 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('admin_users');
     }
