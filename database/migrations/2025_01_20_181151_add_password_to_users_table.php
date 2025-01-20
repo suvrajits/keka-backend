36500a7fbbe56d3b58ac3240e1f27c6d7fe07356
suvrajit <suvrajit.arowana@gmail.com>
2025-01-20 23:44:35 +0530

    migration for password in user

--- database/migrations/2025_01_20_181151_add_password_to_users_table.php
+++ database/migrations/2025_01_20_181151_add_password_to_users_table.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('password')->nullable()->after('email');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('password');
         });
     }
 };
