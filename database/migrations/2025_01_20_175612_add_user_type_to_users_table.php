558b8b025b51a6350656eb45d1733c6407a04dfb
suvrajit <suvrajit.arowana@gmail.com>
2025-01-20 23:28:18 +0530

    updated migration file

--- database/migrations/2025_01_20_175612_add_user_type_to_users_table.php
+++ database/migrations/2025_01_20_175612_add_user_type_to_users_table.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('user_type')->default('player'); // Default to regular user
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('user_type');
         });
     }
 };
