d46f00a99381718b5ab3177d8ea81b556bc68a65
suvrajit <suvrajit.arowana@gmail.com>
2025-01-23 18:35:43 +0530

    updated view for test admin dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-5">
+    <h1 class="text-center">Welcome to Keka Admin Dashboard</h1>
+    <p class="text-center">Manage users, view leaderboards, and more.</p>
+</div>
+@endsection
